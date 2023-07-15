5b769c83779335a2ade2fa63ff391f7f0d894e2b
salcedo12 <saluis7@misena.edu.co>
2023-07-15 13:50:10 -0500

    update responsive

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Container, Row, Col, Card } from 'react-bootstrap';
-import { Link, useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { Link, useParams } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 function CharacterDetails() {
   const { id } = useParams();
   const [character, setCharacter] = useState(null);
 
   useEffect(() => {
-    axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-      .then(response => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/${id}`)
+      .then((response) => {
         setCharacter(response.data);
       })
-      .catch(error => {
-        console.error('Error fetching character details:', error);
+      .catch((error) => {
+        console.error("Error fetching character details:", error);
       });
   }, [id]);
 
@@ -22,23 +25,50 @@ function CharacterDetails() {
   }
 
   return (
-    <Container className='border bg-info p-5'>
-      <Row>
-        <Col xs={12} md={6} className="text-center">
-          <img src={character.image} alt={character.name} className="img-fluid" />
-        </Col>
-        <Col xs={12} md={6}>
-          <h2>{character.name}</h2>
-          <p><strong>ID:</strong> {character.id}</p>
-          <p><strong>Status:</strong> {character.status}</p>
-          <p><strong>Species:</strong> {character.species}</p>
-          <p><strong>Gender:</strong> {character.gender}</p>
-          <p><strong>Origin:</strong> {character.origin.name}</p>
-          <p><strong>Location:</strong> {character.location.name}</p>
-        </Col>
-      </Row>
-      <Link className="text-decoration-none border  bg-success text-white rounded p-2 " to={`/Character`}>volver</Link>
-    </Container>
+    <>
+      <NavBar />
+      <Container className="text-center " style={{height: "80vh", display: "flex", textAlign:"center", justifyContent:"center"}}>
+      <Container className="border bg-info p-5 " >
+        <Row>
+          <Col xs={12} md={6} className="text-center">
+            <img
+              src={character.image}
+              alt={character.name}
+              className="img-fluid"
+            />
+          </Col>
+          <Col xs={12} md={6}>
+            <h2>{character.name}</h2>
+            <p>
+              <strong>ID:</strong> {character.id}
+            </p>
+            <p>
+              <strong>Status:</strong> {character.status}
+            </p>
+            <p>
+              <strong>Species:</strong> {character.species}
+            </p>
+            <p>
+              <strong>Gender:</strong> {character.gender}
+            </p>
+            <p>
+              <strong>Origin:</strong> {character.origin.name}
+            </p>
+            <p>
+              <strong>Location:</strong> {character.location.name}
+            </p>
+          </Col>
+        </Row>
+        <Link
+          className="text-decoration-none border  bg-success text-white rounded p-2 "
+          to={`/Character`}
+        >
+          volver
+        </Link>
+      </Container>
+      </Container>
+      <Footer/>
+    </>
   );
 }
 
